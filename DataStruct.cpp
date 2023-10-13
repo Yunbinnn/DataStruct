fff96e169c54616cbc3f444bdb62c407f49f015a
Yunbinnn <139340918+Yunbinnn@users.noreply.github.com>
2023-10-13 19:47:26 +0900

    (2023.10.13) 원형 연결 리스트

--- DataStruct.cpp
+++ DataStruct.cpp
@@ -1,8 +1,11 @@
 ﻿#include "SingleLinkedList.h"
+#include "DoubleLinkedList.h"
 
 int main()
 {
-	SingleLinkedList<int> sList;
+#pragma region 단방향 연결 리스트
+
+	/*SingleLinkedList<int> sList;
 
 	sList.Push_Front(10);
 	sList.Push_Front(20);
@@ -11,7 +14,19 @@ int main()
 	sList.Pop_Front();
 
 	cout << "sList의 사이즈 : " << sList.Size() << endl;
-	sList.Show();
+	sList.Show();*/
+
+#pragma endregion
+
+#pragma region 양방향 연결 리스트
+
+	DoubleLinkedList<int> dList;
+
+	dList.Push_Back(19);
+	
+	dList.Show();
+
+#pragma endregion
 
 	return 0;
 }
--- DoubleLinkedList.h
+++ DoubleLinkedList.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <iostream>
+using namespace std;
 
 template <typename T>
 class DoubleLinkedList
@@ -25,6 +27,146 @@ public:
 		size = 0;
 	}
 
+	void Push_Back(T _data)
+	{
+		Node* newNode = new Node;
+
+		newNode->data = _data;
+
+		newNode->next = nullptr;
+		newNode->prev = nullptr;
+
+		if (head == nullptr)
+		{
+			head = newNode;
+			tail = newNode;
+		}
+		else
+		{
+			newNode->prev = tail;
+
+			tail->next = newNode;
+
+			tail = newNode;
+		}
+
+		size++;
+	}
+
+	void Push_Front(T _data)
+	{
+		Node* newNode = new Node;
+
+		newNode->data = _data;
+
+		newNode->next = nullptr;
+		newNode->prev = nullptr;
+
+		if (head == nullptr)
+		{
+			head = newNode;
+			tail = newNode;
+		}
+		else if (size == 1)
+		{
+			Node* deleteNode = head;
+
+			head = nullptr;
+			tail = nullptr;
+
+			delete deleteNode;
+		}
+		else
+		{
+			newNode->next = head;
+
+			head->prev = newNode;
+
+			head = newNode;
+		}
+
+		size++;
+	}
+
+	void Pop_Front()
+	{
+		if (head == nullptr)
+		{
+			cout << "데이터가 비어있습니다." << endl;
+			return;
+		}
+		
+		Node* deleteNode = head;
+
+		head = head->next;							
+
+		head->prev = nullptr;
+
+		delete deleteNode;
+
+		size--;
+	}
+
+	void Pop_Back()
+	{
+		if (head == nullptr)
+		{
+			cout << "데이터가 y0." << endl;
+			return;
+		}
+		else
+		{
+			Node* deleteNode = tail;
+
+			tail = tail->prev;
+
+			tail->next = nullptr;
+
+			delete deleteNode;
+		}
+
+		size--;
+	}
+
+	void Insert(int _size, T _data)
+	{
+		if (_size <= 1)
+		{
+			Push_Front(_data);
+			return;
+		}
+
+		Node* newNode = new Node;
+
+		newNode->data = _data;
+
+		newNode->next = nullptr;
+		newNode->prev = nullptr;
+
+		Node* searchNode = head;
+		Node* prevNode;
+
+		for (int i = 1; i < _size; i++)
+		{
+			searchNode = searchNode->next;
+		}
+
+		prevNode = searchNode->prev;
+
+		newNode->next = searchNode;
+		searchNode->prev = newNode;
+
+		prevNode->next = newNode;
+		newNode->prev = prevNode;
+
+		size++;
+	}
+
+	int Size()
+	{
+		return size;
+	}
+	
 	void Show()
 	{
 		Node* currentNode = head->next;
@@ -35,19 +177,15 @@ public:
 		}
 	}
 
-	void Insert(T _data)
+	~DoubleLinkedList()
 	{
-		Node* newNode = new Node;
-		newNode->data = _data;
+		while (head != nullptr)
+		{
+			Node* deleteNode = head;
 
-		Node* currentNode;
-		currentNode = head->next;
+			head = head->next;
 
-		Node* prev = currentNode->prev;
-		prev->next = newNode;
-
-		currentNode->prev = newNode;
-		newNode->next = currentNode;
+			delete deleteNode;
+		}
 	}
-
 };
