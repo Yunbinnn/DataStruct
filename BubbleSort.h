f91e7cfd904d6920b6ab8c365436d9386de8548c
Yunbinnn <139340918+Yunbinnn@users.noreply.github.com>
2023-10-23 15:46:28 +0900

    (2023.10.19) 선택 정렬

---
+++ BubbleSort.h
@@ -0,0 +1,133 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+constexpr auto SIZE = 5;
+
+void BubbleSort(int arr[])
+{
+	for (int i = 0; i < SIZE - 1; i++)
+	{
+		for (int j = 0; j < (SIZE - i) - 1; j++)
+		{
+			if (arr[j] > arr[j + 1])
+				swap(arr[j], arr[j + 1]);
+		}
+	}
+}
+
+#pragma region 연산자 오버로딩
+
+class Vector2
+{
+private:
+	int x;
+	int y;
+
+public:
+	Vector2(int x, int y)
+	{
+		this->x = x;
+		this->y = y;
+	}
+
+	Vector2 operator+(const Vector2& vector2)
+	{
+		Vector2 sum(this->x + vector2.x, this->y + vector2.y);
+
+		return sum;
+	}
+
+	Vector2 operator-(const Vector2& vector2)
+	{
+		Vector2 sum(this->x - vector2.x, this->y - vector2.y);
+
+		return sum;
+	}
+
+	Vector2 operator*(const Vector2& vector2)
+	{
+		Vector2 sum(this->x * vector2.x, this->y * vector2.y);
+
+		return sum;
+	}
+
+	Vector2 operator/(const Vector2& vector2)
+	{
+		Vector2 sum(this->x / vector2.x, this->y / vector2.y);
+
+		return sum;
+	}
+
+	Vector2 operator++()
+	{
+		this->x++;
+		this->y++;
+
+		return *this;
+	}
+
+	Vector2 operator++(int)
+	{
+		Vector2 clone = *this;
+
+		this->x++;
+		this->y++;
+
+		return clone;
+	}
+
+	int GetX() const
+	{
+		return x;
+	}
+
+	int GetY() const
+	{
+		return y;
+	}
+};
+
+#pragma endregion
+
+int main()
+{
+#pragma region 거품 정렬(Bubble Sort)
+	// 서로 인접한 두 원소를 검사하여 정렬하는 알고리즘 입니다.
+
+	// 시간 복잡도 O(n^2)
+
+	/*int arr[SIZE] = { 8,4,9,3,1 };
+
+	cout << "정렬 전 : ";
+
+	for (const int& element : arr)
+	{
+		cout << element << " ";
+	}
+
+	BubbleSort(arr);
+
+	cout << "\n\n정렬 후 : ";
+
+	for (const int& element : arr)
+	{
+		cout << element << " ";
+	}*/
+
+#pragma endregion
+
+#pragma region 연산자 오버로딩
+
+	Vector2 Up(0, 1);
+	Vector2 Right(1, 0);
+
+	Vector2 Sum = Up + Right;
+
+	cout << Sum.GetX() << endl;
+	cout << Sum.GetY() << endl;
+
+#pragma endregion
+
+	return 0;
+}
--- DataStruct.cpp
+++ DataStruct.cpp
@@ -1,112 +1,40 @@
 ﻿#include <iostream>
 using namespace std;
 
-constexpr auto SIZE = 5;
-
-void BubbleSort(int arr[])
-{
-	for (int i = 0; i < SIZE - 1; i++)
-	{
-		for (int j = 0; j < (SIZE - i) - 1; j++)
-		{
-			if (arr[j] > arr[j + 1]) 
-				swap(arr[j], arr[j + 1]);
-		}
-	}
-}
-
-#pragma region 연산자 오버로딩
-
-class Vector2
-{
-private:
-	int x;
-	int y;
-
-public:
-	Vector2(int x, int y)
-	{
-		this->x = x;
-		this->y = y;
-	}
-
-	Vector2 operator+(const Vector2& vector2)
-	{
-		Vector2 sum(this->x + vector2.x, this->y + vector2.y);
-
-		return sum;
-	}
-
-	Vector2 operator-(const Vector2& vector2)
-	{
-		Vector2 sum(this->x - vector2.x, this->y - vector2.y);
-
-		return sum;
-	}
-
-	Vector2 operator*(const Vector2& vector2)
-	{
-		Vector2 sum(this->x * vector2.x, this->y * vector2.y);
-
-		return sum;
-	}
-
-	Vector2 operator/(const Vector2& vector2)
-	{
-		Vector2 sum(this->x / vector2.x, this->y / vector2.y);
-
-		return sum;
-	}
-
-	int GetX() const
-	{
-		return x;
-	}
-
-	int GetY() const
-	{
-		return y;
-	}
-};
-
-#pragma endregion
-
 int main()
 {
-#pragma region 거품 정렬(Bubble Sort)
-	// 서로 인접한 두 원소를 검사하여 정렬하는 알고리즘 입니다.
+#pragma region 선택 정렬
+	// 정렬되지 않은 데이터들에 대해 가장 작은 데이터를
+	// 찾아서 가장 앞에 있는 데이터와 교환하는 알고리즘입니다.
 
 	// 시간 복잡도 O(n^2)
 
-	/*int arr[SIZE] = { 8,4,9,3,1 };
+	int selectBuffer[] = { 6,2,8,1,0 };
 
-	cout << "정렬 전 : ";
+	int size = sizeof(selectBuffer) / sizeof(int);
 
-	for (const int& element : arr)
+	for (int i = 0; i < size; i++)
+	{
+		int min = selectBuffer[i]; // 6
+
+		int select = i;
+
+		for (int j = i + 1; j < size; j++)
+		{
+			if (min > selectBuffer[j])
+			{
+				min = selectBuffer[j];
+				select = j;
+			}
+		}
+
+		swap(selectBuffer[i], selectBuffer[select]);
+	}
+
+	for (const int& element : selectBuffer)
 	{
 		cout << element << " ";
 	}
-
-	BubbleSort(arr);
-
-	cout << "\n\n정렬 후 : ";
-
-	for (const int& element : arr)
-	{
-		cout << element << " ";
-	}*/
-
-#pragma endregion
-	
-#pragma region 연산자 오버로딩
-
-	Vector2 Up(0, 1);
-	Vector2 Right(1, 0);
-
-	Vector2 Sum = Up + Right;
-
-	cout << Sum.GetX() << endl;
-	cout << Sum.GetY() << endl;
 
 #pragma endregion
 
