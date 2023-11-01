3a0f90267df1e68e349e3b9189e6a2d4065e7410
Yunbinnn <139340918+Yunbinnn@users.noreply.github.com>
2023-11-01 19:48:45 +0900

    (2023.11.01) 이진 탐색 트리

--- BinarySearchTree.h
+++ BinarySearchTree.h
@@ -2,8 +2,8 @@
 #include "using_std.h"
 
 #pragma region 이진 탐색 트리(BST)
-	// 임의의 키를 가진 원소를 삽입, 삭제, 검색하는데
-	// 효율적은 알고리즘 입니다.
+// 임의의 키를 가진 원소를 삽입, 삭제, 검색하는데
+// 효율적은 알고리즘 입니다.
 
 template<typename T>
 class BinarySearchTree
@@ -11,48 +11,86 @@ class BinarySearchTree
 private:
 	struct Node
 	{
-		T data;
-		Node* leftChild = nullptr;
-		Node* rightChild = nullptr;
+		T data = NULL;
+		Node* left = nullptr;
+		Node* right = nullptr;
 	};
 
+public:
 	Node* rootNode;
 
-public:
 	inline BinarySearchTree() { rootNode = nullptr; }
 
-	Node* GetRoot() { return rootNode; }
+	Node* GetRoot() const { return rootNode; }
 
-	void Insert(T data, Node* root = rootNode)
+	Node* Insert(T data, Node* root)
 	{
+		if (Find(root, data))
+		{
+			cout << "이미 존재하는 값 입니다." << endl;
+			return root;
+		}
+
 		if (root == nullptr)
 		{
 			root = new Node;
 			root->data = data;
-			root->leftChild = nullptr;
-			root->rightChild = nullptr;
-			return;
+			root->left = nullptr;
+			root->right = nullptr;
 		}
+		else if (root->data > data) root->left = Insert(data, root->left);
+		else if (root->data < data) root->right = Insert(data, root->right);
 
-		if (root->data > data) Insert(data, root->leftChild);
-		else if (root->data < data) Insert(data, root->rightChild);
-		else cout << "이미 존재하는 값 입니다." << endl;
+		return root;
 	}
 
-	T MaxValue(Node* root)
+	Node* Delete(T data, Node* root)
 	{
-		Node* currentNode = root;
+		if (root == nullptr) return root;
+	}
 
-		while(currentNode->rightChild != nullptr) currentNode = currentNode->rightChild;
+	bool Find(Node* root, T data)
+	{
+		if (root == nullptr) return false;
 
-		return currentNode->data;
+		if (root->data == data) return true;
+		else
+			if (root->data > data) return Find(root->left, data);
+			else return Find(root->right, data);
+	}
+
+	T& MaxValue(Node* root)
+	{
+		if (root == nullptr)
+		{
+			cout << "BST가 비어있습니다." << endl;
+			exit(1);
+		}
+
+		if (root->right != nullptr) return MaxValue(root->right);
+		else return root->data;
+	}
+
+	T& MinValue(Node* root)
+	{
+		if (root == nullptr)
+		{
+			cout << "BST가 비어있습니다." << endl;
+			exit(1);
+		}
+
+		if (root->left != nullptr) return MinValue(root->left);
+		else return root->data;
 	}
 
 	void Inorder(Node* root)
 	{
-		Inorder(root->leftChild);
-		cout << root->data << " ";
-		Inorder(root->rightChild);
+		if (root)
+		{
+			Inorder(root->left);
+			cout << root->data << " ";
+			Inorder(root->right);
+		}
 	}
 };
 
--- DataStruct.cpp
+++ DataStruct.cpp
@@ -3,9 +3,27 @@
 
 int main()
 {
+#pragma region 이진 탐색 트리(BST)
+
 	BinarySearchTree<int> bst;
 
-	bst.Insert(10, bst.GetRoot());
+	bst.rootNode = bst.Insert(10, bst.GetRoot());
+
+	bst.Insert(5, bst.GetRoot());
+	bst.Insert(15, bst.GetRoot());
+	bst.Insert(23, bst.GetRoot());
+	bst.Insert(3, bst.GetRoot());
+	bst.Insert(17, bst.GetRoot());
+	bst.Insert(2, bst.GetRoot());
+
+	bst.Inorder(bst.GetRoot());	
+
+	cout << "\n\n";
+
+	cout << "BST Data의 최소 값 : " << bst.MinValue(bst.GetRoot()) << endl;
+	cout << "\nBST Data의 최대 값 : " << bst.MaxValue(bst.GetRoot()) << endl;
+
+#pragma endregion
 
 	return 0;
 }
